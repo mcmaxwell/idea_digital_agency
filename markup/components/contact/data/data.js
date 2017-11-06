c4cd8ee3dc8f9255d190b0005c4d8034d3c91297
Maxim Liutsko <mcmaxwell07@gmail.com>
2017-11-06 15:54:24 +0200

    fix

---
+++ markup/components/contact/data/data.js
@@ -0,0 +1,16 @@
+contact: {
+  ua: {
+    pageName: 'Контакты.',
+    smallTitle: `So what's next?`,
+    bigTitle: 'We’re ready!',
+    linkFormText: 'get in touch',
+    address: '© 2017 Idea Digital Agency Ukraine, Dnipro, M.Malinovskogo 2/4 '
+  },
+  us: {
+    pageName: 'Contact.',
+    smallTitle: `So what's next?`,
+    bigTitle: 'We’re ready!',
+    linkFormText: 'get in touch',
+    address: '© 2017 Idea Digital Agency Ukraine, Dnipro, M.Malinovskogo 2/4 '
+  },
+}
