8a531fae526a8e9760d98aba1c303e8d70ea7abf
Maxim Liutsko <mcmaxwell07@gmail.com>
2017-12-12 11:01:03 +0200

    add blog

---
+++ markup/components/blog/data/data.js
@@ -0,0 +1,268 @@
+blog: {
+  ua: {
+    blogMenu: [
+      {
+        'link': '#all'
+      },
+      {
+        'link': '#seo'
+      },
+      {
+        'link': '#website'
+      },
+      {
+        'link': '#mobileapp'
+      },
+      {
+        'link': '#creativedesign'
+      },
+      {
+        'link': '#platform'
+      },
+      {
+        'link': '#brandstrategy',
+        'class': 'active'
+      },
+      {
+        'link': '#printmedia'
+      },
+      {
+        'link': '#video'
+      },
+      {
+        'link': '#campaigns'
+      }
+    ],
+    blogList: [
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      }
+    ]
+  },
+  us: {
+    blogMenu: [
+      {
+        'link': '#all'
+      },
+      {
+        'link': '#seo'
+      },
+      {
+        'link': '#website'
+      },
+      {
+        'link': '#mobileapp'
+      },
+      {
+        'link': '#creativedesign'
+      },
+      {
+        'link': '#platform'
+      },
+      {
+        'link': '#brandstrategy',
+        'class': 'active'
+      },
+      {
+        'link': '#printmedia'
+      },
+      {
+        'link': '#video'
+      },
+      {
+        'link': '#campaigns'
+      }
+    ],
+    blogList: [
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog4.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      }
+    ]
+  }
+}
---
+++ markup/components/footer/data/data.js
@@ -0,0 +1,8 @@
+footer: {
+  ua: {
+    address: '© 2017 Idea Digital Agency | Україна, Дніпро, І.Акінфієва 18/308'
+  },
+  us: {
+    address: '© 2017 Idea Digital Agency Ukraine, Dnipro, I.Akinfeeva 18/308'
+  },
+}
