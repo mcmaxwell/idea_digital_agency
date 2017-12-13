18368bb14c902d6fc4382c4a6a13ed81c76cb892
Maxim Liutsko <mcmaxwell07@gmail.com>
2017-12-13 16:56:42 +0200

    add blog and build

---
+++ markup/components/more-post/data/data.js
@@ -0,0 +1,25 @@
+morePost: {
+  ua: {
+    title: 'вам также будет интересно',
+    blogList: [
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog1.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog2.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+      {
+        'link': 'blog-page.html',
+        'img': '%=static=%img/assets/blog/blog3.jpg',
+        'title': 'Название поста будет здесь длинное в две строки',
+        'category': '#brandstrategy'
+      },
+    ]
+  }
+}
